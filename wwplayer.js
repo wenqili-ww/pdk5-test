1e961ebf75df339781f05c02230a7429eebd6adc
Wenqi (Eric) <wenqi@wirewax.com>
2020-03-04 18:09:18 -0500

    a test for noConflict

--- wwplayer.js
+++ wwplayer.js
@@ -47005,6 +47005,7 @@ window.reloadWirewax = function(overrides) {
 
 function init_requirejs() {
     wwplayer.require(["jquery", "globals", "apiService", "Core", "modernizr", "pubsub", "underscore", "require-shim", "jquery-jsonp", "requestAnimationFrameShim"], function($, globals, apiService, Core, Modernizr, PubSub, _, requireShim, jqueryJsonp, requestAnimationFrameShim) {
+            $.noConflict(true);
             var self = this;
             // Don't allow Google Web Preview to load the player and cause idiotic errors
             if (globals.IS_GOOGLE_BOT || globals.IS_DEJACLICK_BOT) {
